9d9d6f0b3e72c000b4cb25c69f0e23dd8a70ec7b
Carlton Whitehead <cebesius86@gmail.com>
2018-12-01 11:51:45 -0500

    Add icons

--- src/main/kotlin/org/coner/drs/DigitalRawSheetApp.kt
+++ src/main/kotlin/org/coner/drs/DigitalRawSheetApp.kt
@@ -1,5 +1,7 @@
 package org.coner.drs
 
+import javafx.scene.image.Image
+import javafx.stage.Stage
 import org.coner.style.ConerFxStylesheet
 import tornadofx.*
 import tornadofx.Stylesheet
@@ -7,7 +9,15 @@ import tornadofx.Stylesheet
 class DigitalRawSheetApp : App(
         primaryView = MainView::class,
         stylesheet = org.coner.drs.Stylesheet::class
-)
+) {
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.icons.addAll(
+                listOf(16, 32, 48, 64, 128, 256, 512, 1024)
+                        .map { Image("/coner-icon/coner-icon_$it.png") }
+        )
+    }
+}
 
 class Stylesheet : Stylesheet(ConerFxStylesheet::class) {
 }
