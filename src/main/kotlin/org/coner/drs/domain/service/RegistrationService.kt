9a971cf67fc55112e2291e1096e713c4ad4eb67c
Carlton Whitehead <cebesius86@gmail.com>
2019-03-07 20:09:38 -0500

    Slightly loosen multi-token search, all results on no tokens

--- src/main/kotlin/org/coner/drs/domain/service/RegistrationService.kt
+++ src/main/kotlin/org/coner/drs/domain/service/RegistrationService.kt
@@ -3,26 +3,28 @@ package org.coner.drs.domain.service
 import javafx.collections.ObservableList
 import org.coner.drs.domain.entity.Registration
 import tornadofx.*
+import kotlin.streams.toList
 
 class RegistrationService : Controller() {
 
-    fun search(registrations: ObservableList<Registration>, numbers: String? = ""): List<Registration> {
-        val tokens = numbers?.split(" ") ?: listOf(numbers ?: "")
-        return when (tokens.size) {
+    fun search(registrations: ObservableList<Registration>, numbers: String = ""): List<Registration> {
+        val tokens = numbers.split(" ")
+        val results = when (tokens.size) {
+            0 -> registrations
             1 -> {
                 val tokenChars = tokens.single().toCharArray().distinct()
                 registrations.filter { registration ->
                     tokenChars.all { tokenChar -> registration.numbers.contains(tokenChar) }
                 }
             }
-            2 -> registrations.filter { registration ->
-                registration.numbers == "${tokens[0]} ${tokens[1]}"
-            }
-            3 -> registrations.filter { registration ->
-                registration.numbers == "${tokens[0]} ${tokens[1]} ${tokens[2]}"
+            2, 3 -> registrations.filter { registration ->
+                tokens.all { registration.numbers.contains(it) }
             }
             else -> emptyList()
         }
+        return results.parallelStream()
+                .sorted(compareBy { it.numbers })
+                .toList()
     }
 
 }
--- src/main/kotlin/org/coner/drs/ui/validation/NumbersFieldValidationController.kt
+++ src/main/kotlin/org/coner/drs/ui/validation/NumbersFieldValidationController.kt
@@ -12,20 +12,14 @@ class NumbersFieldValidationController : Controller() {
     val validator: ValidationContext.(String?) -> ValidationMessage? = { numbers ->
         val tokens = numbers?.split(" ") ?: listOf(numbers ?: "")
         when (tokens.size) {
-            2 -> {
-                if (tokens[0].isInt() && !tokens[1].isInt() && tokens.all { it.isNotBlank() })
-                    null
-                else
-                    error("")
-            }
-            3 -> {
-                if (tokens[0].isInt() && !tokens[1].isInt() && !tokens[2].isInt() && tokens.all { it.isNotBlank() })
+            2, 3 -> {
+                if (tokens[0].isInt())
                     null
                 else
                     error("")
             }
             else -> {
-                if (registrationService.search(model.registrations, numbers).size == 1)
+                if (registrationService.search(model.registrations, numbers ?: "").isNotEmpty())
                     null
                 else
                     error("")
