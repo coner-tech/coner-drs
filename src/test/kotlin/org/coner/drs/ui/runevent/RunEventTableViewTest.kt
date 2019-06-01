b78e07d58f9e71ffa1bd2b8cb2557e43c98ff54b
Carlton Whitehead <cebesius86@gmail.com>
2019-06-01 16:37:27 -0400

    Convert RunEventTableViewTest to use TornadoFxViewExtension

--- src/test/kotlin/org/coner/drs/ui/runevent/RunEventTableViewTest.kt
+++ src/test/kotlin/org/coner/drs/ui/runevent/RunEventTableViewTest.kt
@@ -5,55 +5,37 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import org.coner.drs.domain.entity.Run
 import org.coner.drs.domain.payload.InsertDriverIntoSequenceResult
-import org.coner.drs.test.TornadoFxScopeExtension
+import org.coner.drs.test.extension.Init
+import org.coner.drs.test.extension.TornadoFxViewExtension
+import org.coner.drs.test.extension.View
 import org.coner.drs.test.fixture.domain.entity.RunEvents
 import org.coner.drs.ui.alterdriversequence.AlterDriverSequenceController
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.testfx.api.FxRobot
-import org.testfx.api.FxToolkit
 import org.testfx.assertions.api.Assertions
 import tornadofx.*
 
-@ExtendWith(TornadoFxScopeExtension::class, MockKExtension::class)
+@ExtendWith(TornadoFxViewExtension::class, MockKExtension::class)
 class RunEventTableViewTest {
 
-    // TODO: refactor and clean this up
-
-    private lateinit var model: RunEventTableModel
+    @View
     private lateinit var view: RunEventTableView
+    private lateinit var model: RunEventTableModel
     private lateinit var controller: RunEventTableController
 
-    private lateinit var app: App
-    private lateinit var robot: FxRobot
-
-    @BeforeEach
-    fun before(scope: Scope) {
-        val stage = FxToolkit.registerPrimaryStage()
-        app = FxToolkit.setupApplication { object : App() {
-            override var scope = scope
-        } } as App
+    @Init
+    fun init(scope: Scope) {
         find<RunEventModel>(scope).apply {
             event = RunEvents.basic()
         }
         controller = find(scope)
         model = find(scope)
         view = find(scope)
-        robot = FxRobot()
-        robot.interact { view.openWindow() }
     }
-
-    @AfterEach
-    fun after() {
-        FxToolkit.cleanupApplication(app)
-        FxToolkit.cleanupStages()
-    }
-
 
     @Test
-    fun `When user inserts driver into sequence it should select it`() {
+    fun `When user inserts driver into sequence it should select it`(robot: FxRobot) {
         val event = find<RunEventModel>().event
         robot.interact {
             event.runs += Run(
