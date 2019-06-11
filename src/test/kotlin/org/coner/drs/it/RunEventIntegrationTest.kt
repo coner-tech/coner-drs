0be4e08d75a2b00022e511d086449cbf66072915
Carlton Whitehead <cebesius86@gmail.com>
2019-06-11 19:42:41 -0400

    Remove pre-TornadoFxAppExtension integration test

--- src/test/kotlin/org/coner/drs/it/RunEventIntegrationTest.kt
+++ src/test/kotlin/org/coner/drs/it/RunEventIntegrationTest.kt
@@ -7,29 +7,65 @@ import assertk.assertions.index
 import assertk.assertions.isEqualTo
 import assertk.assertions.prop
 import org.assertj.core.api.Assumptions
+import org.coner.drs.DigitalRawSheetApp
 import org.coner.drs.domain.entity.Event
 import org.coner.drs.domain.entity.Run
 import org.coner.drs.io.gateway.EventGateway
+import org.coner.drs.it.util.FilesystemFixture
+import org.coner.drs.it.util.IntegrationTestApp
+import org.coner.drs.test.extension.*
+import org.coner.drs.test.extension.App
 import org.coner.drs.test.fixture.integration.crispyfish.classdefinition.Thscc2019V0Classes
 import org.coner.drs.test.fixture.integration.crispyfish.event.Thscc2019Points1
 import org.coner.drs.test.page.fast.FastChooseEventPage
+import org.coner.drs.test.page.fast.FastStartPage
 import org.coner.drs.test.page.real.RealRunEventPage
+import org.coner.drs.ui.chooseevent.ChooseEventController
 import org.coner.drs.ui.chooseevent.ChooseEventModel
+import org.coner.drs.ui.chooseevent.ChooseEventTableView
+import org.coner.drs.ui.main.MainView
+import org.junit.After
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.io.TempDir
+import org.testfx.api.FxRobot
+import org.testfx.api.FxToolkit
 import tornadofx.*
+import java.nio.file.Path
 import java.time.LocalDate
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-class RunEventIntegrationTest : BaseIntegrationTest() {
+@ExtendWith(TornadoFxAppExtension::class)
+class RunEventIntegrationTest {
 
-    private lateinit var event: Event
-    private lateinit var page: RealRunEventPage
+    @TempDir
+    lateinit var tempDir: Path
 
-    @BeforeEach
-    fun beforeEach() {
-        val classDefinitionFile = Thscc2019V0Classes.produceClassDefinitionFile(crispyFishDatabase!!)
-        val eventControlFile = Thscc2019Points1.produceEventControlFile(crispyFishDatabase!!, classDefinitionFile)
+    @App
+    lateinit var app: tornadofx.App
+    lateinit var folders: FilesystemFixture
+    lateinit var event: Event
+    lateinit var page: RealRunEventPage
+
+    @Init
+    fun init() {
+        folders = FilesystemFixture(tempDir)
+    }
+
+    @SetupApp
+    fun setupApp() = IntegrationTestApp(folders.appConfigBasePath)
+
+    @Start
+    fun start(robot: FxRobot) {
+        println(folders.root)
+        val startPage = FastStartPage(find(app.scope), robot)
+        startPage.setRawSheetDatabase(folders.rawSheetDatabase)
+        startPage.setCrispyFishDatabase(folders.crispyFishDatabase.toFile())
+        startPage.clickStartButton()
+        val classDefinitionFile = Thscc2019V0Classes.produceClassDefinitionFile(folders.crispyFishDatabase.toFile())
+        val eventControlFile = Thscc2019Points1.produceEventControlFile(folders.crispyFishDatabase.toFile(), classDefinitionFile)
         val event = Event(
                 date = LocalDate.parse("2019-03-03"),
                 name = "RunEventIntegrationTest",
@@ -38,20 +74,14 @@ class RunEventIntegrationTest : BaseIntegrationTest() {
                         eventControlFile = eventControlFile.file
                 )
         )
-        val app = this.app!!
-        val robot = this.robot!!
-        val latch = CountDownLatch(1)
+        val latch = CountDownLatch(2)
         val fastChooseEventPage = FastChooseEventPage(robot)
-        find<ChooseEventModel>(app.scope).events.onChange { latch.countDown() }
-        robot.interactNoWait {
-            Thread.sleep(160)
-            val gateway: EventGateway = find(app.scope)
-            gateway.save(event)
-        }
-        latch.await()
+        find<ChooseEventModel>(app.scope).dockedProperty.onChangeOnce { runLater { latch.countDown() } }
+        find<ChooseEventTableView>(app.scope).root.items.onChange { runLater { latch.countDown() } }
+        FX.runAndWait { find<EventGateway>(app.scope).save(event) }
+        latch.await(1, TimeUnit.SECONDS)
         fastChooseEventPage.selectEvent { it.id == event.id }
         fastChooseEventPage.clickStartButton()
-
         this.event = find<EventGateway>(app.scope).list().first()
         this.page = RealRunEventPage(robot)
     }
